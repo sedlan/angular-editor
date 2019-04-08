ab59a3af1e531b8228914a72624a81b9b185a607
Jovan Sedlan <jovan.sedlan@comdata.rs>
2019-04-08 14:22:41 +0200

    Changed coding style, added offline Font Awesome v5, unified HTML output, added afterInit event (needs to be registered to use)

--- src/lib/angular-editor.component.spec.ts
+++ src/lib/angular-editor.component.spec.ts
@@ -1,29 +1,29 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {AngularEditorComponent} from './angular-editor.component';
-import {AngularEditorToolbarComponent} from "./angular-editor-toolbar.component";
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { AngularEditorComponent } from './angular-editor.component';
+import { AngularEditorToolbarComponent } from './angular-editor-toolbar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AngularEditorComponent', () => {
-  let component: AngularEditorComponent;
-  let fixture: ComponentFixture<AngularEditorComponent>;
+    let component: AngularEditorComponent;
+    let fixture: ComponentFixture<AngularEditorComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [ FormsModule, HttpClientModule ],
-      declarations: [AngularEditorComponent, AngularEditorToolbarComponent, ]
-    })
-      .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule, HttpClientModule],
+            declarations: [AngularEditorComponent, AngularEditorToolbarComponent,]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AngularEditorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AngularEditorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/lib/angular-editor.module.ts
+++ src/lib/angular-editor.module.ts
@@ -1,15 +1,12 @@
-import {NgModule} from '@angular/core';
-import {AngularEditorComponent} from './angular-editor.component';
-import {AngularEditorToolbarComponent} from "./angular-editor-toolbar.component";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {CommonModule} from "@angular/common";
+import { NgModule } from '@angular/core';
+import { AngularEditorComponent } from './angular-editor.component';
+import { AngularEditorToolbarComponent } from './angular-editor-toolbar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [
-    CommonModule, FormsModule, ReactiveFormsModule
-  ],
-  declarations: [AngularEditorComponent, AngularEditorToolbarComponent],
-  exports: [AngularEditorComponent, AngularEditorToolbarComponent]
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    declarations: [AngularEditorComponent, AngularEditorToolbarComponent],
+    exports: [AngularEditorComponent, AngularEditorToolbarComponent]
 })
-export class AngularEditorModule {
-}
+export class AngularEditorModule { }
